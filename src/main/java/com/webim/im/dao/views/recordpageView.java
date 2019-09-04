1cbc2d0c714c9d6e19d0c962597a18d2c6731b71
zhangwei <894295467@qq.com>
2019-09-04 17:01:42 +0800

    IM后台代码功能变动

--- src/main/java/com/webim/im/dao/views/recordpageView.java
+++ src/main/java/com/webim/im/dao/views/recordpageView.java
@@ -3,12 +3,14 @@ package com.webim.im.dao.views;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.webim.im.view.Page;
 
 import lombok.Data;
 
 @Data
 public class recordpageView {
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date firstdate; // 和用户聊天的第一条信息发送时间
     private Page page;
 }
